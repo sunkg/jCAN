d6a7a7f5ddf1b27bf758e9600286b5cd84c3884c
sunkg <Kaicong.Sun@ipvs.uni-stuttgart.de>
2022-11-28 16:52:43 +0800

    Update train.py

--- release/train.py
+++ release/train.py
@@ -381,17 +381,17 @@ if __name__ == '__main__':
                         help='Number of channels in sensitivity network')
     parser.add_argument('--sens_steps', type=int, default=4, \
                         help='Number of steps in initial sensitivity network')
-    parser.add_argument('--GT', action='store_true', \
-                        help='if there is GT') 
-    parser.add_argument('--exchange_Modal', action='store_true', \
+    parser.add_argument('--GT', type=str2bool, default = True, \
+                        help='if there is GT, default is True') 
+    parser.add_argument('--exchange_Modal', type=str2bool, default = False, \
                         help='Augment data by exchanging the order of protocals, default is False') 
-    parser.add_argument('--ds_ref', action='store_true', \
-                        help='Use gradient map of reference image as additional input') 
+    parser.add_argument('--ds_ref', type=str2bool, default = True, \
+                        help='Use gradient map of reference image as additional input, default is True') 
     parser.add_argument('--SR_scale', type=int, default=1, \
                         help='resoluton enhancement factor')
-    parser.add_argument('--is_Kspace', action='store_true', \
-                        help='Raw data is in Kspace') 
-    parser.add_argument('--is_Unet', action='store_true', \
+    parser.add_argument('--is_Kspace', type=str2bool, default = False, \
+                        help='Raw data is in Kspace, default is False') 
+    parser.add_argument('--is_Unet', type=str2bool, default = True, \
                         help='Use Unet for estimation of senstivity map, alternative is ResNet') 
     parser.add_argument('--mask', metavar='type', \
                         choices=['equispaced', 'random'], \
